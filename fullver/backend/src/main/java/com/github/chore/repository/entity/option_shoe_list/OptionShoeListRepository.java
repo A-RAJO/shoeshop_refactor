7a64789b82abe51643c039da6d6dcf85ad217b5a
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2025-10-04 15:24:58 +0900

    rename : OptionCartRepository to OptionShoeListRepository

---
+++ fullver/backend/src/main/java/com/github/chore/repository/entity/option_shoe_list/OptionShoeListRepository.java
@@ -0,0 +1,10 @@
+package com.github.chore.repository.entity.option_shoe_list;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface OptionShoeListRepository extends JpaRepository<OptionShoeList, Integer> {
+
+}
