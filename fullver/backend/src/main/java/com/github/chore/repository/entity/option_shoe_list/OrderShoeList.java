fbd1f465e166da7ac8adc14dba7d154b2c402cda
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2025-10-07 15:06:20 +0900

    fix : 잘못된 엔티티명 OptionShoeList(F) to OrderShoeList(T) 수정

--- fullver/backend/src/main/java/com/github/chore/repository/entity/option_shoe_list/OrderShoeList.java
+++ fullver/backend/src/main/java/com/github/chore/repository/entity/option_shoe_list/OrderShoeList.java
@@ -1,7 +1,6 @@
 package com.github.chore.repository.entity.option_shoe_list;
 
 import com.github.chore.repository.entity.order_list.OrderList;
-import com.github.chore.repository.entity.order_list.OrderStatus;
 import com.github.chore.repository.entity.shoe_option.ShoeOption;
 import jakarta.persistence.*;
 
@@ -17,7 +16,7 @@ import java.time.LocalDateTime;
 @Builder
 @Table(name = "order_shoe_list")
 @Entity
-public class OptionShoeList {
+public class OrderShoeList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_shoe_list_id", nullable = false)
@@ -32,54 +31,44 @@ public class OptionShoeList {
     private OrderList orderListId;
 
     @Column(name = "quantity", nullable = false)
-    private Integer quantity=1;
+    private Integer quantity;
 
     @Column(name = "unit_price",nullable = false)
     private BigDecimal unitPrice;
 
-    @Builder.Default
     @Column(name = "option_additional_price")
-    private BigDecimal optionAdditionalPrice = BigDecimal.valueOf(0);
+    private BigDecimal optionAdditionalPrice;
 
-    @Builder.Default
     @Column(name = "discount_amount")
-    private BigDecimal discountAmount = BigDecimal.valueOf(0);
+    private BigDecimal discountAmount;
 
     @Column(name = "final_unit_price")
     private BigDecimal finalUnitPrice;
 
-
     @Column(name = "total_amount")
     private BigDecimal totalAmount;
-
 
     @Builder.Default
     @Column(name = "is_cancellable")
     private Boolean isCancellable = true;
 
-
     @Builder.Default
     @Column(name = "order_shoe_status",nullable = false)
-    private OrderShoeStatus orderShoeStatus=OrderShoeStatus.ORDERED ;
-
+    private OrderShoeStatus orderShoeStatus=OrderShoeStatus.ORDERED; // DB에도 설정되어 있지만 가독성 향상을 위해 디폴트값 명시
 
     @Column(name = "snapshot_shoe_name",nullable = false)
     private String snapshotShoeName;
 
-
     @Column(name = "snapshot_brand",nullable = false, length = 50)
     private String snapshotBrand;
-
 
     @Column(name = "snapshot_color",nullable = false, length = 50)
     private String snapshotColor;
 
-
     @Column(name = "snapshot_size",nullable = false)
     private Integer snapshoeSize;
 
-
-    @Column(name = "created_at", nullable = false, updatable = false) // 자동 입력 기능 필요 : @LastModifiedDate와 Auditing 설정
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @Column(name = "updated_at",nullable = false)
