88f8c9a70e657f3822b0f966ab8898218365ef13
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2025-10-07 15:00:40 +0900

    fix : repository 정의 잘못 수정 @Repository 빈 등록 테이블명,아이디타입 순서로 고침(잘못: 아이디타입,테이블명)

--- fullver/backend/src/main/java/com/github/chore/repository/entity/inventory/InventoryRepository.java
+++ fullver/backend/src/main/java/com/github/chore/repository/entity/inventory/InventoryRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InventoryRepository extends JpaRepository<Integer,Inventory> {
+public interface InventoryRepository extends JpaRepository<Inventory,Integer> {
 }
