aec53e3bc6d352739af94f5bcdd3a50181aa4aba
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2025-10-07 14:59:27 +0900

    feat : Inquiry entity and repository 구현  - 테이블명 cs_inquiry, 엔티티명 Inquiry  - 고객 cs 내역 기록, 일정 시간 이후 개인정보 마스킹 처리(DB)

---
+++ fullver/backend/src/main/java/com/github/chore/repository/entity/inquiry/InquiryRepository.java
@@ -0,0 +1,8 @@
+package com.github.chore.repository.entity.inquiry;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InquiryRepository extends JpaRepository<Inquiry,Integer> {
+}
