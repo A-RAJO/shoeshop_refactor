ebee8ad1c8de8a968c10a2fe6eda7a4a1897704b
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2025-10-12 17:40:45 +0900

    feat : findByEmail을 optional로 구현 - UserDetails - loadUserByUsername에서 email로 유저를 찾기 위함. - null 체크, optional이 아니면 서비스단에서 orElseThrow 사용 못함

--- fullver/backend/src/main/java/com/github/chore/repository/entity/user/UserRepository.java
+++ fullver/backend/src/main/java/com/github/chore/repository/entity/user/UserRepository.java
@@ -1,13 +1,16 @@
 package com.github.chore.repository.entity.user;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     boolean existsByUserName(String userName);
     boolean existsByEmail(String email);
-
+    Optional<User> findByEmail(String email); // null 체크, optional이 아니면 서비스단에서 orElseThrow 사용 못함
 }
+
+
