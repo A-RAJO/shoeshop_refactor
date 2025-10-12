aa043a128a4664b59debfec47229695d23392bd7
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2025-10-12 17:57:01 +0900

    feat : UserDetailsService의 구현체 CustomUserDetailService
    loadUserByUsername에서 직접 구현해야 하는 것
    1. DB에서 email로 유저 찾기 (userRepository.findByEmail)
    2. 없으면 예외 던지기 (UsernameNotFoundException)
    3. 있으면 UserDetails 객체로 변환해서 리턴하기

--- fullver/backend/src/main/java/com/github/chore/service/security/CustomUserDetailService.java
+++ fullver/backend/src/main/java/com/github/chore/service/security/CustomUserDetailService.java
@@ -1,34 +1,32 @@
-//package com.github.chore.service.security;
-//
-//import com.github.chore.repository.UserDetails.CustomUserDetails;
-//import com.github.chore.repository.entity.user.User;
-//import com.github.chore.repository.entity.user.UserRepository;
-//import com.github.chore.web.exceptions.NotFoundException;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//@Primary
-//@RequiredArgsConstructor
-//@Service
-//public class CustomUserDetailService implements UserDetailsService {
-//
-//    private final UserRepository userRepository;
-//    @Override
-//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-//        User user=userRepository.findByEmail(email).orElseThrow(()->new NotFoundException("email에 해당하는 user가 없습니다."));
-//
-//        CustomUserDetails customUserDetails=CustomUserDetails.builder()
-//                .userId(user.getUserId())
-//                .email(user.getEmail())
-//                .password(user.getPassword())
-//                .authorities(user.getUser_role().stream().map(userRole -> userRole.getRoleName().toString()).toList())
-//                .build();
-//
-//        return customUserDetails;
-//
-//    }
-//}
+package com.github.chore.service.security;
+
+import com.github.chore.exception.UserNotFoundException;
+import com.github.chore.repository.entity.user.User;
+import com.github.chore.repository.entity.user.UserRepository;
+import com.github.chore.repository.entity.user_details.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Primary
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)  // req 정보에서 DB와 일치하는 데이터 찾기(아이디-이메일로)
+                .orElseThrow(() -> new UserNotFoundException("email에 해당하는 user가 없습니다."));
+
+
+        return CustomUserDetails.fromUser(user); // 이메일로 찾은 user를 user_detail로 변환
+    }
+
+
+}
