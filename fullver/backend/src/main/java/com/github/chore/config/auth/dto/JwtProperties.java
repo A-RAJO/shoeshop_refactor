0322700918c2d77c1e7e69ba6380c75f330b11b6
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2025-10-12 20:07:41 +0900

    refactor : JwtProperties 'jwt' to 'spring.jwt' - prefix값 구체화함

--- fullver/backend/src/main/java/com/github/chore/config/auth/dto/JwtProperties.java
+++ fullver/backend/src/main/java/com/github/chore/config/auth/dto/JwtProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "jwt")
+@ConfigurationProperties(prefix = "spring.jwt")
 @Getter
 @Setter
 public class JwtProperties {
