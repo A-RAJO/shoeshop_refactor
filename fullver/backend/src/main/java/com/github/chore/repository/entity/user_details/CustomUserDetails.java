7e8b3ff81c70c08ea153e67982591772d9ea2de5
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2025-10-12 18:00:04 +0900

    feat : UserDetails의 구현체CustomUserDetails - fromUser : user와 user_detail 분리, user를 인증을 위해 user_detail로 변환 - 역할 담당을 분명히 하기 위해 분리가 필요함. - DB와 매핑하는 엔티티는 User, 인증을 위해 User 정보를 가지고 오는 클래스는 CustomUserDetails

--- fullver/backend/src/main/java/com/github/chore/repository/entity/user_details/CustomUserDetails.java
+++ fullver/backend/src/main/java/com/github/chore/repository/entity/user_details/CustomUserDetails.java
@@ -1,62 +1,70 @@
-//package com.github.chore.repository.UserDetails;
-//
-//import lombok.*;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import java.util.Collection;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Getter
-//@Builder
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@ToString
-//public class CustomUserDetails implements UserDetails {
-//
-//    private Integer userId;
-//    private String email;
-//    private String password;
-//    private List<String> authorities;
-//
-//    public Integer getUserId() {
-//        return userId;
-//    }
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return this.password;
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return this.email;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
-//}
+package com.github.chore.repository.entity.user_details;
+
+import com.github.chore.repository.entity.user.User;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CustomUserDetails implements UserDetails {
+
+    private Integer userId;
+    private String email;
+    private String password;
+    private List<String> authorities;
+
+    public static CustomUserDetails fromUser(User user){
+        List<String> authorities = user.getUserRoles()
+                .stream().map(userRole -> String.valueOf(userRole.getRole().getRoleType()))
+                .toList();
+    return CustomUserDetails.builder()
+                .userId(user.getUserId())
+                .email(user.getEmail())
+                .authorities(authorities)
+            .build();
+    }
+
+    @Override //계정이 가진 권한 목록을 리턴
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override //계정의 비밀번호를 리턴
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override //계정의 아이디 등 식별자 리턴
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override // 계정의 만료 여부 리턴
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override // 계정이 잠겨있는지 리턴
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override // 비밀번호가 만료되었는지 리턴
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override // 계정이 활성화 상태인지
+    public boolean isEnabled() {
+        return true;
+    }
+}
