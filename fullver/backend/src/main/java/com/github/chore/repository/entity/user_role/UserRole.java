c4e87c55ee068e89c9ec2d9ee724db7f1f7d3e50
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2025-10-12 17:37:29 +0900

    feat : 양방향 매핑 구현 - rename : user_role to UserRole - FetchType.LAZY 추가 - User -> UserDetail로 변환하기 위해 userRole이 필요함. 리포지토리를 구현하고 양방향 매핑을 해서 User에서 Role을 불러낼 수 있도록 함

--- fullver/backend/src/main/java/com/github/chore/repository/entity/user_role/UserRole.java
+++ fullver/backend/src/main/java/com/github/chore/repository/entity/user_role/UserRole.java
@@ -12,17 +12,17 @@ import lombok.*;
 @AllArgsConstructor
 @Table(name = "user_role")
 @Entity
-public class user_role {
+public class UserRole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_role_id",nullable = false)
     private Integer userRoleId;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id",nullable = false)
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "role_id",nullable = false)
     private Role role;
 
